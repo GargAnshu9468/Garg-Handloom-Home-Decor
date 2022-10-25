2caa2432643dbac8dfe71046c25cdd383e730820
Anshu Garg <a.kgarg9050@gmail.com>
2022-10-25 08:17:19 +0530

    second commit

--- handloom/settings.py
+++ handloom/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = eval(os.environ.get("DEBUG"))
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["garghandloom.herokuapp.com", "*"]
 
 # Application definition
 
